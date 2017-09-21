943f9d2cb67846b9e15bd99fd65a3809fbb9ca2b
Demi Lee <demilee.to@gmail.com>
2017-09-21 11:24:28 -0400

    total_times add

--- total_times.rb
+++ total_times.rb
@@ -1,10 +1,14 @@
 dice = [1, 2, 3, 4, 5, 6]
 dice2 = [1, 2, 3, 4, 5, 6]
-counter = 0
+counter = {}
+
 dice.each do |die|
   dice2.each do |die2|
-    counter += 1
-    puts "Dice Roll: #{die} #{die2} Total: #{die + die2}"
+    total = die + die2
+    (counter[total] ||= []) << 1
   end
 end
-puts "There are #{counter} possible permutations."
+
+counter.each do |x, y|
+  puts "#{x} occurs #{y.count} times."
+end
