8ce03dc36412693fb57532ffc732999f8c27c6c1
Demi Lee <demilee.to@gmail.com>
2017-09-21 10:44:02 -0400

    add dice

---
+++ dice.rb
@@ -0,0 +1,14 @@
+dice = [1, 2, 3, 4, 5, 6]
+rolls1 = []
+rolls2 = []
+
+def roll2sum(x, y, z)
+  1.times do
+    y << x[Random.rand(6)]
+    z << x[Random.rand(6)]
+  end
+  puts "You rolled #{y[0]} and #{z[0]}."
+  puts "Your total is #{y[0] + z[0]}."
+end
+
+roll2sum(dice, rolls1, rolls2)
