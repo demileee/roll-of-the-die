07e97aa4e4fd7a78404901e37c0c7e2677d745c5
Demi Lee <demilee.to@gmail.com>
2017-09-21 10:54:43 -0400

    how_many permutations

---
+++ total_times.rb
@@ -0,0 +1,10 @@
+dice = [1, 2, 3, 4, 5, 6]
+dice2 = [1, 2, 3, 4, 5, 6]
+counter = 0
+dice.each do |die|
+  dice2.each do |die2|
+    counter += 1
+    puts "Dice Roll: #{die} #{die2} Total: #{die + die2}"
+  end
+end
+puts "There are #{counter} possible permutations."
