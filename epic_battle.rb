0ae764e714330dab8f2a4c6d862d1cd9fef05c07
Demi Lee <demilee.to@gmail.com>
2017-09-21 22:41:05 -0400

    account that highest of attacker rolls will be in play, changes results

--- epic_battle.rb
+++ epic_battle.rb
@@ -32,15 +32,42 @@ def play()
     dice.each do |roll2|
       dice.each do |roll3|
         puts "#{roll1} #{roll2} #{roll3} \s\s #{roll2} #{roll3}"
-        if roll1 > roll2
+
+        att_1 = 0
+        att_2 = 0
+
+        if roll1 > roll2 && roll1 > roll3
+          att_1 = roll1.to_i
+          if roll2 > roll3
+            att_2 = roll2.to_i
+          elsif roll3 > roll2
+            att_2 = roll3.to_i
+          end
+        elsif roll2 > roll1 && roll2 > roll3
+          att_1 = roll2.to_i
+          if roll1 > roll3
+            att_2 = roll1.to_i
+          elsif roll3 > roll1
+            att_2 = roll3.to_i
+          end
+        elsif roll3 > roll1 && roll3 > roll2
+          att_1 = roll3.to_i
+          if roll1 > roll2
+            att_2 = roll1.to_i
+          elsif roll2 > roll1
+            att_2 = roll2.to_i
+          end
+        end
+
+        if att_1 > roll2
           y = y - 1
-        elsif roll2 > roll1
+        elsif roll2 > att_1
           x = x - 1
         end
 
-        if roll2 > roll3
+        if att_2 > roll3
           y = y - 1
-        elsif roll3 > roll2
+        elsif roll3 > att_2
           x = x - 1
         end
       end
@@ -52,8 +79,9 @@ def play()
   puts "Attacker's final army count: #{x}"
   puts "Defender's final army count: #{y}"
 
-  puts "\nFor every 100 armies the attacker loses, the defender loses 100 armies."
+  ratio = y.to_f/x
 
+  puts "\nFor every 100 armies the attacker loses, the defender loses #{(ratio*100).ceil} armies."
 end
 
 play()
